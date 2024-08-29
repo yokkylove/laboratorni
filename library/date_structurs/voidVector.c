29b518f4170e36657940464749c48c80fdaca483
yokkylove <117486978+yokkylove@users.noreply.github.com>
2024-08-29 23:19:50 +0300

    memory usage of vectorVoid

--- library/date_structurs/voidVector.c
+++ library/date_structurs/voidVector.c
@@ -1,2 +1,38 @@
 #include "voidVector.h"
 
+vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
+    vectorVoid result = {(int *) malloc(baseTypeSize * n), 0, n,
+                         baseTypeSize};
+    if (result.data == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+    return result;
+}
+
+void reserveV(vectorVoid *v, const size_t newCapacity) {
+    if (newCapacity == 0)
+        v->data = NULL;
+    else
+        v->data = (int *) realloc(v->data, v->baseTypeSize *
+                                           newCapacity);
+    if (v->data == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+    if (v->size > newCapacity)
+        v->size = newCapacity;
+    v->capacity = newCapacity;
+}
+
+void shrinkToFitV(vectorVoid *v) {
+    reserveV(v, v->size);
+}
+
+void clearV(vectorVoid *v) {
+    v->size = 0;
+}
+
+void deleteVectorV(vectorVoid *v) {
+    free(v->data);
+}
--- library/date_structurs/voidVector.h
+++ library/date_structurs/voidVector.h
@@ -2,6 +2,9 @@
 #define GG_VOIDVECTOR_H
 
 #include <limits.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <malloc.h>
 
 typedef struct vectorVoid {
     void *data; // указатель на нулевой элемент вектора
@@ -13,5 +16,20 @@ typedef struct vectorVoid {
                         // если вектор хранит float -
                         // то поле baseTypeSize = sizeof(float)
 } vectorVoid;
+
+//возвращает структуру-дескриптор вектор из n значений.
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+//изменяет количество памяти, выделенное под хранение элементов вектора.
+void reserveV(vectorVoid *v, const size_t newCapacity);
+
+//освобождает память, выделенную поднеиспользуемые элементы.
+void shrinkToFitV(vectorVoid *v);
+
+//удаляет элементы из контейнера, но не освобождает выделенную память.
+void clearV(vectorVoid *v);
+
+//освобождает память, выделенную вектору.
+void deleteVectorV(vectorVoid *v);
 
 #endif //GG_VOIDVECTOR_H
