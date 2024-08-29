6f3f793b9cf08d3f9eb511f1f57dd0e4a6fe7a7c
yokkylove <117486978+yokkylove@users.noreply.github.com>
2024-08-30 02:29:54 +0300

    6 task block

--- library/date_structurs/lab_18.c
+++ library/date_structurs/lab_18.c
@@ -5,6 +5,7 @@ __FILE__, __FUNCTION__, __LINE__)
 #include <ctype.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
 
 typedef struct WordDescriptor {
     char *begin; // позиция начала слова
@@ -372,12 +373,64 @@ void test_replace_task5() {
     test_replace_firstWordIsNotInString();
 }
 
+//Определяет, упорядочены ли лексикографически слова данного предложе ния.
+bool areWordsOrdered(char *string) {
+    WordDescriptor word1;
+    WordDescriptor word2;
+    if (getWord(string, &word1)) {
+        word2 = word1;
+        while (getWord(string, &word1)) {
+            if (areWordsEqual(word1, word2) < 0)
+                return false;
+            word2 = word1;
+            string = word1.end;
+        }
+        return true;
+    } else {
+        return true;
+    }
+}
+
+void test_areWordsOrdered_stringIsEmpty() {
+    char s[] = "";
+    assert(areWordsOrdered(s) == true);
+}
+
+void test_areWordsOrdered_oneWordInString() {
+    char s[] = "dream";
+    assert(areWordsOrdered(s) == true);
+}
+
+void test_areWordsOrdered_NotorderedString() {
+    char s[] = "pass op";
+    assert(areWordsOrdered(s) == true);
+}
+
+void test_areWordsOrdered_TwoWordsEqual() {
+    char s[] = "pass pass";
+    assert(areWordsOrdered(s) == true);
+}
+
+void test_areWordsOrdered_orderedString() {
+    char s[] = "a ab baaa ca cd";
+    assert(areWordsOrdered(s) == true);
+}
+
+void test_areWordsOrdered_task6() {
+    test_areWordsOrdered_stringIsEmpty();
+    test_areWordsOrdered_oneWordInString();
+    test_areWordsOrdered_NotorderedString();
+    test_areWordsOrdered_TwoWordsEqual();
+    test_areWordsOrdered_orderedString();
+}
+
 int main(){
     test_removeNonLetters();
     test_removeExtraSpaces();
     test_digitToStartLetterToEndt();
     test_replacesNumbersWithSpaces_task4();
     test_replace_task5();
+    test_areWordsOrdered_task6();
 
     return 0;
 }
